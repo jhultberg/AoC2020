be4931d57ae9ec2b3b0aa7b02ad304a432b78645
Johanna Hultberg <johanna.chultberg@gmail.com>
2020-12-01 20:03:25 +0100

    Day 1

--- aoc/day1.py
+++ aoc/day1.py
@@ -1,7 +1,22 @@
+def two(input):
+    for i in input:
+        for j in input:
+            if i+j == 2020
+                return i*j
+    return 0
+
+def three(input):
+    for i in input:
+        for j in input:
+            fir k in input:
+                if i+j+k == 2020
+                return i*j*k
+    return 0
+
+
 def solve(path):
     with open(path) as ins:
         input = ins.readlines()
-        print(input)
-        a=0
-        b=0
+        a = two(input)
+        b = three(input)
         return (a, b)
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,15 +1,14 @@
-from aoc.day1 import calc_sum, recursive_calc_sum, solve
+from aoc.day1 import two, three, solve
+
+test_data = [1721, 979, 366, 299, 675, 1456]
+
+def test_two():
+    assert two(test_data) == 514579
 
 
-def test_calc_sum():
-    assert calc_sum(1969) == 654
-    assert calc_sum(100756) == 33583
-
-
-def test_recursive_calc_sum():
-    assert recursive_calc_sum(1969, 0) == 966
-    assert recursive_calc_sum(100756, 0) == 50346
+def test_three():
+    assert three(test_data) == 241861950
 
 
 def test_solve():
-    assert solve("data/day1.txt") == (3412496, 5115845)
+    assert solve("data/day1.txt") == (211899, 275765682)
