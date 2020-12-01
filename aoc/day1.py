7ebbf7b6c61437c65a2f2567db3c3fc2980bb2ec
Johanna Hultberg <johanna.chultberg@gmail.com>
2020-12-01 20:17:40 +0100

    rename input to data

--- aoc/day1.py
+++ aoc/day1.py
@@ -1,14 +1,14 @@
-def two(input):
-    for i in input:
-        for j in input:
+def two(data):
+    for i in data:
+        for j in data:
             if i+j == 2020
                 return i*j
     return 0
 
-def three(input):
-    for i in input:
-        for j in input:
-            for k in input:
+def three(data):
+    for i in data:
+        for j in data:
+            for k in data:
                 if i+j+k == 2020
                    return i*j*k
     return 0
@@ -16,7 +16,7 @@ def three(input):
 
 def solve(path):
     with open(path) as ins:
-        input = ins.readlines()
-        a = two(input)
-        b = three(input)
+        data = ins.readlines()
+        a = two(data)
+        b = three(data)
         return (a, b)
